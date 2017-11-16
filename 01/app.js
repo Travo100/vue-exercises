430cf4af3fe40d5ed25a7cdf71a539051d649994
John Desrosiers <johndrunner@yahoo.com>
2017-11-16 06:12:42 -0800

    my solution to ex 01

--- 01/app.js
+++ 01/app.js
@@ -1,3 +1,13 @@
 new Vue({
-  
+  el: '#app',
+  data: {
+    name: 'John Desrosiers',
+    age: 35,
+    googleImg: "https://www.skisafari.com/sites/ss/files/vail-powder-skier-trees-credit-cody-downard.jpg",
+  },
+  methods: {
+    randFloat: function getRandomFloat() {
+      return Math.random();
+    }
+  }
 });
