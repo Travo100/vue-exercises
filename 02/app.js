0f0f367652edaf9e8dc3a3b3f6ffb813916b6ba8
John Desrosiers <johndrunner@yahoo.com>
2017-11-19 07:16:10 -0800

    exercise 2 solution

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,14 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        }
-    });
+  el: '#app',
+  data: {
+    value: '',
+  },
+  methods: {
+    setValue: function setValueFromInput(event) {
+      this.value = event.target.value;
+    },
+    showAlert: function showAlert() {
+      alert('Get in the choppa!');
+    },
+  },
+});
