bc6a204987b6b0349a2d1e4909a28879ad24cb38
John Desrosiers <johndrunner@yahoo.com>
2017-11-19 06:31:28 -0800

    fix linting errors exercise 01

--- 01/app.js
+++ 01/app.js
@@ -3,11 +3,11 @@ new Vue({
   data: {
     name: 'John Desrosiers',
     age: 35,
-    googleImg: "https://www.skisafari.com/sites/ss/files/vail-powder-skier-trees-credit-cody-downard.jpg",
+    googleImg: 'https://www.skisafari.com/sites/ss/files/vail-powder-skier-trees-credit-cody-downard.jpg',
   },
   methods: {
     randFloat: function getRandomFloat() {
       return Math.random();
-    }
-  }
+    },
+  },
 });
