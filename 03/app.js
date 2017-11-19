cd7210a3ee2aa12a9716ee1351ee0e6720303ea8
John Desrosiers <johndrunner@yahoo.com>
2017-11-19 08:12:44 -0800

    solved ex 3

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,18 @@
 new Vue({
-  el: '#exercise',
+  el: '#app',
   data: {
     value: 0,
+  },
+  computed: {
+    result() {
+      return this.value < 37 ? 'not there yet' : 'done';
+    },
+  },
+  watch: {
+    result() {
+      setTimeout(() => {
+        this.value = 0;
+      }, 5000);
+    },
   },
 });
